837a9424d44cab5c3f2e81ab156d29abf578c22d
fandev-001 <46459245+fandev-001@users.noreply.github.com>
2021-02-26 16:08:06 -0600

    fix bg not working
    add style for right/left alignment

--- common.js
+++ common.js
@@ -28,8 +28,8 @@ jQuery(document).ready(function()
     // Grab style element's sheet
     var styleSheet = styleEl.sheet;
 
+    jQuery(".wp-block-group.parallax-background, .wp-block-group.parallax-foreground").css('background-image', function(index){
 
-    jQuery(".wp-block-group.parallax-background", ".wp-block-group.parallax-foreground").css('background-image', function(index){
         //create a unique identifier
         // stick a class name to this element
         const unique_id = 'vips-' + Date.now();
@@ -37,18 +37,30 @@ jQuery(document).ready(function()
         //find the first background image
         //make it invisible
         //grab its src value
-        var first_image = jQuery(this).find( '.background img' ).first();
+        var first_image = jQuery(this).find( '.background img:not(.p-omit, .alignright, .alignleft, .p-omit img, .alignright img, .alignleft img)' ).first();
+
+        //first() will return a length of 0 if not found
+        //grab the next available image
+        if(first_image.length == 0)
+        {
+            first_image = jQuery(this).find( 'img:not(.p-omit, .alignright, .alignleft, .p-omit img, .alignright img, .alignleft img)' ).first();
+
+        }
         if(first_image)
         {
-            jQuery(first_image).css( "opacity", "0" );
+            //jQuery(first_image).css( "opacity", "0" );
 
         	index = jQuery(first_image).attr( "src" );
 
-            //set this::before background-image to the first_image.src
-            styleSheet.insertRule('.' + unique_id + '::before { background-image:url(' + index + ') }', styleSheet.cssRules.length);
+            if(index)
+            {
+                //set this::before background-image to the first_image.src
+                styleSheet.insertRule('.' + unique_id + '::before { background-image:url(' + index + ') }', styleSheet.cssRules.length);                
+            }
         }
 
         //return  'url(' + index + ')';//old method was to set the bg for the parent element, we use ::before now
         return 'none';
     });
 });
+
